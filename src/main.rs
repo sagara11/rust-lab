c53ce1501c49daa43d894abe54de1b15c418f1a4
Thong Nguyen <thong.nguyen@savvycomsoftware.com>
2022-11-11 14:04:40 +0700

    mpsc

--- src/main.rs
+++ src/main.rs
@@ -30,15 +30,36 @@ use std::time::Duration;
 fn main() {
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
-    });
-    for i in 0..5 {
-        let received = rx.try_recv().unwrap();
-        println!("Got: {}", received);
-        println!("{}", i * 2);
-    }
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
 
-    // new_thread.join().unwrap();
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
