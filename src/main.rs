3d26278deef3b69c55326ab773733e4fee6ad4f6
Minh Thong <thiensangkdtt@gmail.com>
2022-11-16 23:52:27 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
-use std::cell::RefCell;
-use std::sync::mpsc;
-use std::sync::{Arc, Mutex};
-use std::thread;
+use rust_lab::gui::{Button, Draw, Screen};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
 
 fn main() {
-    let counter = Arc::new(Mutex::new(0));
-    let mut handles = vec![];
+    let screen = Screen {
+        components: vec![Box::new(String::from("Hi"))],
+    };
 
-    for _ in 0..10 {
-        let counter = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            let mut num = counter.lock().unwrap();
+    screen.run();
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
 
-            *num += 1;
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    println!("Result: {}", *counter.lock().unwrap());
+    screen.run();
 }
